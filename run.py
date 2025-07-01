afed6893e6d9ad9877580ed5d4f6d9b652905601
dadukhankevin <danieljlosey@gmail.com>
2025-07-01 10:33:51 -0500

    Removed over complicated parts so that I just change the run command on digital ocean.

--- run.py
+++ run.py
@@ -1,11 +1,3 @@
-import os
-import sys
-
-# Ensure project root is in Python path
-project_root = os.path.dirname(os.path.abspath(__file__))
-if project_root not in sys.path:
-    sys.path.insert(0, project_root)
-
 from app import create_app
 
 app = create_app()
